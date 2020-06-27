a6c6f28b12fdf31b67dc9c2a708026fcddae5a70
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-06-27 03:05:17 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = new Object({keys: '0'});
 
 function updatePlaylist(playlist, artistName, songTitle) {
 
-  playlist.artistName = songTitle;
+  playlist[artistName] = songTitle;
 
   return playlist
 
