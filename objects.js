33fed21b6f39b08148eba3406152735a8ac115ea
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-06-27 02:47:47 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,13 @@
 var playlist = new Object({jessieReyez: 'Intruders'});
+
+var updatePlaylist = (playlist, artistName, songTitle) => {
+
+  var artistName = 'kaceyMusgroves';
+
+  var songTitle = 'Space Cowboy';
+
+  playlist.artistName = songTitle;
+
+  return playlist;
+  
+}
