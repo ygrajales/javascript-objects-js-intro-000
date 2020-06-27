a3026ae0fe927226644089d4c22b0ae05459a7e8
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-06-27 03:06:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,6 +4,14 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
   playlist[artistName] = songTitle;
 
-  return playlist
+  return playlist;
 
+}
+
+function removeFromPlaylist(playlist, artistName) {
+
+  delete playlist[artistName];
+
+  return playlist;
+  
 }
